62a7f830a188e520c7a4ea9fc294fc6a1f7f6343
Bo L <cyborgxxi@gmail.com>
2019-01-09 20:27:06 +0700

    Update SQLServercode.sql

--- SQLServercode.sql
+++ SQLServercode.sql
@@ -11,3 +11,17 @@ SET @VAL_Y = (SELECT YEAR(END_DATE) FROM Val_Date)
 
 DECLARE @SWITCH_ROUNDING AS INT
 SET @SWITCH_ROUNDING = 1
+
+-- Unpivoting a table
+-- See below link for more info (pivot and unpivot functions in SQL Server)
+-- https://docs.microsoft.com/en-us/sql/t-sql/queries/from-using-pivot-and-unpivot?view=sql-server-2017
+
+SELECT VendorID, Employee, Orders  
+FROM   
+   (SELECT VendorID, Emp1, Emp2, Emp3, Emp4, Emp5  
+   FROM pvt) p  
+UNPIVOT  
+   (Orders FOR Employee IN   
+      (Emp1, Emp2, Emp3, Emp4, Emp5)  
+)AS unpvt;  
+GO  
